35510ea5f879424df7fde0793239a77de2c0a45c
Magomed Abduragimov <magish_abu@mail.ru>
2022-12-18 00:22:25 +0200

    fix cli.js

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,6 @@
-import readlineSync from "readline-sync";
+import readlineSync from 'readline-sync';
 
- export const giveMeAName = () => {
-    const userName = readlineSync.question(`May I have your name? `);
-    console.log(`Hello, ${userName}!`);
+export default () => {
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
 };
-
